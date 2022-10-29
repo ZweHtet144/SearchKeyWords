e7048e09968252a42533d62496a65db5a69f68e4
ZweHtet <zwewaiyanhtet@acedatasystems.com>
2022-10-29 14:39:48 +0630

    -

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # SearchKeyWords
+# Reference Source Link
+1-https://ideone.com/kEDoXA
+2-https://www.cambiaresearch.com/articles/6/extract-keywords-from-a-search-string-in-csharp
+
+
+#Link-1 Code
+
+
+using System;
+using System.Text.RegularExpressions;
+using System.IO;
+public class Test
+{
+	public static void Main()
+	{
+		var searchString = "to find";
+		var pattern = @"\b" + searchString + @"\b";    //searchString is passed in.
+		Regex rgx = new Regex(pattern);
+		var sentence = "I need to find a string in this sentence!";
+		Match match = rgx.Match(sentence);
+		if (match.Success)
+		{
+    		Console.WriteLine(match.Value);
+		}
+	}
+}
+
+#Link-2 Code
+
+public static string[] GetSearchWords(string text)
+{
+	string pattern = @"\S+";
+	Regex re = new Regex(pattern);
+
+	MatchCollection matches = re.Matches(text);
+	string[] words = new string[matches.Count];
+	for (int i=0; i<matches.Count; i++)
+	{
+		words[i] = matches[i].Value;
+	}
+	return words;
+}
+
---
+++ SearchKeywordsApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SearchKeywordsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paragraph = @"Even though apple is not an excellent source of dietary fiber (it ranks as a &quot;good&quot; source in our
+                                WHFoods Rating System), the fiber found in apple may combine with other apple nutrients to
+                                provide you with the kind of health benefits you would ordinarily only associate with much higher
+                                amounts of dietary fiber. These health benefits are particularly important in prevention of heart
+                                disease through healthy regulation of blood fat levels. Recent research has shown that intake of
+                                apples in their whole food form can significantly lower many of our blood fats.";
+            
+            var searchString = "apple";
+            var pattern = @"\b" + searchString + @"\b";
+            Regex rgx = new Regex(pattern);
+
+            MatchCollection matches = rgx.Matches(paragraph);
+            string[] keywords = new string[matches.Count];
+            for (int i = 0; i < matches.Count; i++)
+            {
+                keywords[i] = matches[i].Value;
+                Console.WriteLine(keywords[i]);
+            }
+        }
+    }
+}
